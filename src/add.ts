af8aa9da83ee51774318855f3af23401d2c7b728
liujie <liujie09@kuaishou.com>
2020-12-28 16:22:47 +0800

    初始化

---
+++ src/add.ts
@@ -0,0 +1,4 @@
+import { IndexInfo } from 'typescript';
+
+const Index: (a: number, b: number) => number = (a: number, b: number) => a + b;
+export default Index;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * 当前工具函数库版本
+ */
+export const version: string = '1.0.0';
+export { default as add } from './add';
+export { default as isColor } from './is-color';
